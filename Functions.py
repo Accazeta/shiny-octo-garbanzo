aa27b66186b8787e6390e628c97e0d4360dbf95a
accazeta <f.panzeri24@studenti.unibg.it>
2022-07-03 15:57:17 +0200

    Adjusted custom rng with plots and everything

--- Functions.py
+++ Functions.py
@@ -1,9 +1,25 @@
+import random
+
 def weight_builder(length, m1, q1, m2, q2):
     half = length / 2
     result = [0] * length  # builds an empty list of zeros
     for i in range(0, length):
         if i <= half:
-            result[i] = m1 * (i / length) + q1  # /length is used to normalize to 1
+            result[i] = m1 * (i / length) + q1
         else:
             result[i] = m2 * (i / length) + q2
     return result
+
+
+def custom_pdf_number_generator(population, weigths):
+    return random.choices(population, weigths)
+
+def adjusted_number_generator(max_int, population, weight):
+    # Genero un numero
+    num1 = custom_pdf_number_generator(population, weight)
+    # Genero un secondo numero
+    num2 = custom_pdf_number_generator(population, weight)
+    # Moltiplico i due numeri generati tra loro (chiamo questo numero "prod")
+    prod = num1[0] * num2[0]  # le parentesi quadre servono perchè si tratta di liste unitarie
+    # Calcolo il resto intero della divisione tra prod e il numero casuale massimo che voglio estrarre
+    return prod % max_int
--- PrimoTest.py
+++ PrimoTest.py
@@ -1,11 +1,36 @@
-import random as r
+import random
 import Costants as c
 import Functions as f
 from matplotlib import pyplot as plt
 
 len = 1024
 
+population = range(0, len)
+
 weightList = f.weight_builder(len, c.m1, c.q1, c.m2, c.q2)
 
+print(weightList)
 plt.plot(weightList)
-plt.show()  # Shows the plot of the distribution created
+plt.show()
+
+# generate n numbers using the probability density function just created
+n = 100000
+
+dict_results1 = dict.fromkeys(population, 0)
+
+for i in range(0,n):
+    # estraggo n numeri e li stampo giusto per vedere se funziona.
+    dict_results1[f.custom_pdf_number_generator(population, weightList)[0]] += 1
+
+plt.plot(dict_results1.keys(), dict_results1.values())
+plt.show()
+
+dict_results2 = dict.fromkeys(population, 0)
+maxInt = 50
+for i in range(0, n):
+    dict_results2[f.adjusted_number_generator(maxInt, population, weightList)] += 1
+
+smaller_dict = dict((key,value) for key, value in dict_results2.items() if key < maxInt)
+
+plt.plot(smaller_dict.keys(), smaller_dict.values())
+plt.show()
